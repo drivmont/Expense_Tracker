e0cf7e39a6ddd3fa854479c543b2577e4f9e8900
domik <u209453172@byui.edu>
2021-03-06 16:24:54 -0800

    Added Log.i for buttons on MainActivity

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void addExpense(View view) {
         Intent intent = new Intent(this, Expenses.class);
+        Log.i("MESSAGE", "Adding Expense");
         startActivity(intent);
     }
 
     public void addAlert(View view) {
         Intent intent = new Intent(this, Alert.class);
+        Log.i("MESSAGE", "Adding Alert");
         startActivity(intent);
     }
 
     public void getSummary(View view) {
         Intent intent = new Intent(this, Summary.class);
+        Log.i("MESSAGE", "Getting Summary");
         startActivity(intent);
     }
+
+
 }
