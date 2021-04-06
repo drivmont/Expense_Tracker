e34915ba3e566b6fd4565271d134361dfee43e49
Diego Rivera <rivmont.diego@gmail.com>
2021-04-06 14:28:44 -0400

    Comments in Expenses
    Comment the expenses class to explain how it works

--- app/src/main/java/com/example/finalproject/Expenses.java
+++ app/src/main/java/com/example/finalproject/Expenses.java
@@ -21,6 +21,7 @@ public class Expenses extends AppCompatActivity implements AdapterView.OnItemSel
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+        //Create spinner object to add string values to the spinner options for the Alert UI.
         Spinner spinner = findViewById(R.id.expenseCategory);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -31,7 +32,9 @@ public class Expenses extends AppCompatActivity implements AdapterView.OnItemSel
     }
 
     public void addExpenses(View view){
+        //Create db handler to add new entries to the database or query it.
         DatabaseHandlerExpense db = new DatabaseHandlerExpense(this);
+        //Get input values from user
         EditText name = (EditText) findViewById(R.id.expenseName);
         Spinner category = (Spinner) findViewById(R.id.expenseCategory);
         EditText date = (EditText) findViewById(R.id.expenseDate);
@@ -40,6 +43,7 @@ public class Expenses extends AppCompatActivity implements AdapterView.OnItemSel
         String c = category.getSelectedItem().toString();
         String d = date.getText().toString();
         String a = amount.getText().toString();
+        //Verify that none of the fields is empty, if any is empty then show a toast.
         if (n.matches("") || c.matches("") || d.matches("") || a.matches("")  ){
             Toast.makeText(this,"Fill all the information to add an expense", Toast.LENGTH_SHORT).show();
             return;
@@ -47,25 +51,25 @@ public class Expenses extends AppCompatActivity implements AdapterView.OnItemSel
         else {
             db.addExpense(new Expense(n, c, d, a));
 
-            // Reading all contacts
+            // Reading all expenses
             Log.d("Reading: ", "Reading all contacts..");
             List<Expense> contacts = db.getAllExpenses();
 
             for (Expense cn : contacts) {
                 String log = "Id: " + cn.getID() + " ,Name: " + cn.getName() + " ,Category: " +
                         cn.getCategory() + ",Date: " + cn.getDate() + "Amount: " + cn.getAmount();
-                // Writing Contacts to log
+                // Writing expenses to log
                 Log.d("Name: ", log);
             }
         }
     }
-
+    //Method for the spinner when an item is selected.
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
        // String text = parent.getItemAtPosition(position).toString();
        // Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
     }
-
+    //Method for the spinner when an item is not selected.
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
